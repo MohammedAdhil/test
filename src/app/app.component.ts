efcf69d393d0fcd84f4a2e0d7e9e0c98f0f92dde
MohammedAdhil <18gcs09@meaec.edu.in>
2023-02-23 20:41:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angularproject';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit() {
+    console.log('Life Cyle Hook with spontaneous response.');
+  }
+  // tinyAlert() {
+  //   Swal.fire('Hey there!');
+  // }
+  // successNotification() {
+  //   Swal.fire('Hi', 'We have been informed!', 'success');
+  // }
+  alertConfirmation() {
+    Swal.fire({
+  
+      // text: 'This process is irreversible.',
+      // icon: 'warning',
+      // title: "An input!",
+      // text: "Enter your details and address :",
+      // input: 'text',
+      // inputPlaceholder: 'Enter Your Address',
+      html:
+    '<input id="swal-input1" class="swal2-input" placeholder="Enter Your Name">' +
+    '<input id="swal-input2" class="swal2-input" placeholder="Enter Your Phone Number">'+
+    '<input id="swal-input1" class="swal2-input" placeholder="Enter Your Address">' ,
+
+      // text: "Write something interesting:",
+      // input: 'text',
+
+
+      showCancelButton: true,
+      confirmButtonText: 'Confirm',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Order Placed!', 'Product ordered successfully.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your Order has been cancelled.', 'error');
+      }
+    });
+  }
 }
